1e08bf8f19762396ea54840df58035bdd2ca088a
Danial Tavakoli <dani.sepahani@gmail.com>
2024-04-11 00:06:29 +0330

    remove suspend from functions in Dao

--- app/src/main/java/com/danialtavakoli/omdb/model/db/MovieDao.kt
+++ app/src/main/java/com/danialtavakoli/omdb/model/db/MovieDao.kt
@@ -10,15 +10,15 @@ import com.danialtavakoli.omdb.model.data.MovieDetails
 @Dao
 interface MovieDao {
     @Query("SELECT * FROM movieTable")
-    suspend fun getMoviesList(): List<Movie>?
+    fun getMoviesList(): List<Movie>?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertMovies(movies: List<Movie>)
+    fun insertMovies(movies: List<Movie>)
 
     @Query("SELECT * FROM movieDetailsTable WHERE imdbID = :imdbID")
-    suspend fun getMovieDetails(imdbID: String): MovieDetails?
+    fun getMovieDetails(imdbID: String): MovieDetails?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertMovieDetails(movieDetails: MovieDetails)
+    fun insertMovieDetails(movieDetails: MovieDetails)
 
 }
