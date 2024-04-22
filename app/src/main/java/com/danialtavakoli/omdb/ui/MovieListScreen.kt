5fd618b03979809c73387dd670ca7697771f183e
Danial Tavakoli <dani.sepahani@gmail.com>
2024-04-23 02:01:05 +0330

    add searchBar not completed

--- app/src/main/java/com/danialtavakoli/omdb/ui/MovieListScreen.kt
+++ app/src/main/java/com/danialtavakoli/omdb/ui/MovieListScreen.kt
@@ -1,5 +1,6 @@
 package com.danialtavakoli.omdb.ui
 
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,18 +13,28 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.danialtavakoli.omdb.model.data.Movie
@@ -38,19 +49,39 @@ fun MovieListScreen(
     context: MainActivity,
     modifier: Modifier = Modifier,
 ) {
+    var title by remember { mutableStateOf(movieTitle) }
+
     val moviesList by viewModel.moviesList.collectAsState()
 
     LaunchedEffect(key1 = Unit) {
         val isInternetConnected = NetworkChecker(context).isInternetConnected
         viewModel.fetchMovies(
-            title = movieTitle, isInternetConnected = isInternetConnected
+            title = title, isInternetConnected = isInternetConnected
         )
         if (!isInternetConnected && moviesList.isEmpty()) context.showToast("Internet not connected!")
     }
 
-    LazyColumn(modifier = modifier) {
-        items(items = moviesList) { movie ->
-            MovieListItem(movie = movie, onItemClick = onItemClick)
+    Column(modifier = modifier) {
+        SearchBar(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            onSearch = { newTitle ->
+                title = newTitle
+                suspend {
+                    Log.v("testX", title)
+                    viewModel.fetchMovies(
+                        title = newTitle,
+                        isInternetConnected = NetworkChecker(context).isInternetConnected
+                    )
+                }
+            }
+        )
+
+        LazyColumn {
+            items(items = moviesList) { movie ->
+                MovieListItem(movie = movie, onItemClick = onItemClick)
+            }
         }
     }
 }
@@ -96,5 +127,48 @@ fun MovieListItem(
                 )
             }
         }
+    }
+}
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    onSearch: (String) -> Unit
+) {
+    var text by remember { mutableStateOf(TextFieldValue()) }
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = Icons.Rounded.Search,
+            contentDescription = null,
+            modifier = Modifier.size(24.dp),
+            tint = Color.Gray
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+        BasicTextField(
+            value = text,
+            onValueChange = { text = it },
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 8.dp),
+            singleLine = true,
+
+            )
+
+        Icon(
+            painter = painterResource(id = androidx.core.R.drawable.ic_call_answer),
+            contentDescription = null,
+            modifier = Modifier
+                .size(24.dp)
+                .clickable {
+                    onSearch(text.text)
+                }
+        )
     }
 }
